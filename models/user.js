5d24c186b7c65a7737ebe01f6734b2dda1900fe0
giovanni <mormagli@gmail.com>
2018-06-11 13:43:38 -0500

    added user schema

---
+++ models/user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fullname: String,
+  username:{type: String, required: true, unique: true},
+  password:{type: String, require: true}
+
+});
+
+
+userSchema.set('toObject', {
+  virtuals: true,     // include built-in virtual `id`
+  versionKey: false,  // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+    delete ret.password;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema, 'users');
